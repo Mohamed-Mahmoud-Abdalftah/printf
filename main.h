48a69f73f3bfbdaf7e759de857a3d8b029d7c695
Mohamed Mahmoud <36283687+Mohamed-Mahmoud-Abdalftah@users.noreply.github.com>
2024-07-27 22:52:15 +0300

    add'

--- main.h
+++ main.h
@@ -1,49 +1,37 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef PRINTF
+#define PRINTF
 #include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
-
-
-
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "main.h"
+#include <stdarg.h>
 /**
- * struct format - match the conversion specifiers for printf
- * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
- *
+ * hexa - struct
+ * @i: integer
+ * @c: characters
  */
-
-typedef struct format
+typedef struct rot13
 {
-	char *id;
-	int (*f)();
-} convert_match;
+	char c;
+} rt;
+/**
+ * prf -prf
+ * @c: characters
+ * @f: pointer to function
+ */
+typedef struct prf
+{
+	char c;
+	int (*f)(va_list p);
 
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _putchar(char c);
+} pr;
 int _printf(const char *format, ...);
+int print_int(va_list i);
+int print_string(va_list s);
+int print_char(va_list c);
+int _putchar(char c);
+int print_percent(va_list c);
 
 #endif
